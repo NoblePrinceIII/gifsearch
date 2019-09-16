b2509d71d1a54548125507d80c7c71ab4cfabc73
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-09-16 15:55:44 -0700

    added a few changes

--- app.py
+++ app.py
@@ -9,35 +9,32 @@ def index():
     """Return homepage."""
     query = request.args.get("search gif")
     results = get_results(query)
-    # TODO: Extract query term from url
 
-    # TODO: Make 'params' dict with query term and API key
-
-    # TODO: Make an API call to Tenor using the 'requests' library
-
-    # TODO: Get the first 10 results from the search results
-
-    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
     return render_template("index.html", results = results)
 
 
+@app.route('/submit')
 def get_results(query):
     # set the apikey and limit
     apikey = "3OGJ9M5CUDUK"  # test value
     limit = 10
 
-# get the top 10 GIFs for the search term
+    # get the top 10 GIFs for the search term
     r = requests.get(
     "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (query, apikey, limit))
+
 
     if r.status_code == 200:
         # load the GIFs using the urls for the smaller GIF sizes
         top_10gifs = json.loads(r.content)
+        top_10gifs = top_10gifs["results"]
     else:
         top_10gifs = None
 
-    return top_10gifs
+
+    return render_template("index.html", top_10gifs = top_10gifs, )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
