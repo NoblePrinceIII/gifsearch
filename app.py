ee3ea51a7b89f45be25cf67d014f9588b5284af5
kmurata798 <kmurata798@gmail.com>
2019-09-18 16:12:01 -0700

    fixed app.py

--- app.py
+++ app.py
@@ -12,28 +12,22 @@ def index():
         "q" : query,
         "key" : "3OGJ9M5CUDUK"
     }
+    data = None
 
-    r = requests.get( "https://api.tenor.com/v1/search?", params = payload)
-    data = r.json()
+    r = requests.get( "https://api.tenor.com/v1/search", params = payload)
+    if r.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        data = r.json()
+        data = data["results"]
+    else:
+        data = None
     return render_template("index.html", data = data)
 
 
-@app.route('/submit')
-def get_results(query):
+
     # set the apikey and limit  
 
     # get the top 10 GIFs for the search term
-
-
-    if r.status_code == 200:
-        # load the GIFs using the urls for the smaller GIF sizes
-        top_10gifs = json.loads(r.content)
-        top_10gifs = top_10gifs["results"]
-    else:
-        top_10gifs = None
-
-
-    return render_template("base.html", top_10gifs = top_10gifs)
 
 
 if __name__ == '__main__':
