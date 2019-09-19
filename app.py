b6584b88ea0621e59df5ba56cf71ca7c0bb6bf25
kmurata798 <kmurata798@gmail.com>
2019-09-18 22:29:00 -0700

    allignment adjusted

--- app.py
+++ app.py
@@ -4,31 +4,28 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """Return homepage."""
     query = request.args.get("search_gif")
     payload = {
-        "q" : query,
-        "key" : "3OGJ9M5CUDUK"
+        "q": query,
+        "key": "3OGJ9M5CUDUK"
     }
     data = None
 
-    r = requests.get( "https://api.tenor.com/v1/search", params = payload)
+    r = requests.get("https://api.tenor.com/v1/search", params=payload)
     if r.status_code == 200:
         # load the GIFs using the urls for the smaller GIF sizes
         data = r.json()
         data = data["results"]
     else:
         data = None
-    return render_template("index.html", data = data)
+    return render_template("index.html", data=data)
 
-
-
-    # set the apikey and limit  
+    # set the apikey and limit
 
     # get the top 10 GIFs for the search term
-
-
 if __name__ == '__main__':
     app.run(debug=True)
